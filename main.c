7abe40f1642dc02021ee32e4c59006225bf2d2fa
Dj4t9n <pandeyjatin47@gmailn47@gmail.com>
2021-05-31 01:38:11 +0530

    STATUS:PENDING ============== TODO   - implement rotation logic in rotate function   - rotate <1000, 0, 0> and take dot product with position vector of each point

--- main.c
+++ main.c
@@ -1,85 +1,32 @@
 #include <stdio.h>
 #include <math.h>
-#include <unistd.h>
-#include <signal.h>
 
-typedef struct {
-	float x, y, z;
-}Co3;
-
-typedef struct {
-	float x, y;
-} Co2;
-
-Co2 project(Co3 Coordinate){
-	Co2 res;
-	res.x = Coordinate.x;		
-	res.y = Coordinate.y;		
-	return res;
-}
-
-Co3 rotate_X(Co3 Coordinate){
-	//impl rotation matrix
-}
-
-Co3 rotate_Y(Co3 Coordinate){
-	//impl rotation matrix
-}
-
-Co3 rotate_Z(Co3 Coordinate){
-	//impl rotation matrix
-}
-
-Co2 rotate(Co2 Coordinates, float theta){
-	float temp_x, temp_y;
-	temp_x = Coordinates.x*cos(theta) - Coordinates.y*sin(theta);	
-	temp_y = Coordinates.y*cos(theta) + Coordinates.x*sin(theta);	
-	Co2 aux_coordinate = {temp_x, temp_y};
-	return aux_coordinate;
-}
-
-void handle_sig(void){
-	printf("bye\n");
-	exit(0);
-}
-
-void clear_src(void){
-	printf("\033[2J");
-}
-
-void move_curs(int x, int y){
-	printf("\033[%d;%dH", x, y);
-}
+void rotate(float *x , float *y, float *z, float theta);
 
 int main(void){
-	float theta = 0.0f; 
-	Co3 A = {8.0f, 8.0f, 8.0f};
-	Co3 B = {8.0f, 16.0f, 8.0f};
-	Co3 C = {16.0f, 16.0f, 8.0f};
-	Co3 D = {16.0f, 8.0f, 8.0f};
-	Co3 E = {8.0f, 8.0f,  16.0f};
-	Co3 F = {8.0f, 16.0f, 16.0f};
-	Co3 G = {16.0f, 16.0f,16.0f};
-	Co3 H = {16.0f, 8.0f, 16.0f};
-	Co2 a = project(A);
-	Co2 b = project(B);
-	Co2 c = project(C);
-	Co2 d = project(D);
-	int k = 0;
+	int x, y, z;
+	char output[2500];
+	float theta = 0.0f;
 	while(1){
-		k = (k + 1)%20;
-		signal(SIGINT, handle_sig);
-		clear_src();
-		usleep(200000);
-		theta += M_PI/180;
-		a = rotate(a, theta);
-		b = rotate(b, theta);
-		c = rotate(c, theta);
-		d = rotate(d, theta);
-		printf("a = (%f, %f), b = (%f, %f), c = (%f, %f), d = (%f, %f)\n\n\n", a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
-		move_curs(k+2, k+2);
-		printf(".");
+		//generate the cube centered at origin
+		//
+		for (y = -1000; y <= 1000; y++){
+			for (z = -1000; z <= 1000; z++){
+				x = 1000;
+				//rotate in all three directions
+				rotate(&x, &y, &z, theta);
+				//calculate the dot product of <x, y, z> and surface normal
+				//but how do i calculate surface normal
+				// ----------Potential Method-----------
+				// 	1> rotate <1000, 0, 0> with same values as other points in this cube
+				//	2> assuming light source at origin (<0, 0, 0>), find position vector of every point WRT origin 
+				//	3> take dot product of Position vector and surface normal
 
+				
+			}
+		}
+		theta += 0.01f;
+		
 	}
 	return 0;
 }
