0995226acd5d8809117d8c35ff1075d64af2c7aa
Dj4t9n <pandeyjatin47@gmailn47@gmail.com>
2021-05-31 03:08:31 +0530

    STATUS:Fixed some mistakes GOAL still remains the same

--- main.c
+++ main.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 void rotate(float *x , float *y, float *z, float theta);
 
 int main(void){
 	float x, y, z;
+	int i = 0;
 	char output[2500];
 	float theta = 0.0f;
 	while(1){
 		//generate the cube centered at origin
 		//
-		for (y = -100; y <= 100; y++){
-			for (z = -100; z <= 100; z++){
-				x = 100;
+		printf("\t\t\t%d\n", ++i);
+		printf("------------------------------\n");
+		usleep(2000000);
+		for (y = -50; y <= 50; y++){
+			for (z = -50; z <= 50; z++){
+				x = 50;
 				//rotate in all three directions
-				rotate(&x, &y, &z, theta);
-				printf("%f, %f, %f\n", x, y, z);
+				float x_temp = (float)x;
+				float y_temp = (float)y;
+				float z_temp = (float)z;
+
+				rotate(&x_temp, &y_temp, &z_temp, theta);
+				printf("%f, %f, %f\n", x_temp, y_temp, z_temp);
 				//calculate the dot product of <x, y, z> and surface normal
 				//but how do i calculate surface normal
 				// ----------Potential Method-----------
@@ -26,7 +35,7 @@ int main(void){
 				
 			}
 		}
-		theta += 0.01f;
+		theta += 1.0f;
 		
 	}
 	return 0;
