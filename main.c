714b879081d460d216b1092ec50c325a65d67299
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-03 17:42:35 +0530

    Implement Projection function

--- main.c
+++ main.c
@@ -2,70 +2,79 @@
 #include <unistd.h>
 
 void rotate(float *x , float *y, float *z, float theta);
-
+void project(float *x , float *y, float *z, float theta);
+// TESTING
+// --------
+// 	project function should take inputs and print '.' at projected x and projected y
+//
 int main(void){
 	float x, y, z, x_temp, y_temp, z_temp;
-	char output[2500];
+	char output[100];
 	float theta = 0.0f;
 	while(1){
-		printf("working");
-		for (y = -30; y <= 30; y++){
-			for (z = -30; z <= 30; z++){
-				x = 30;
+		for (y = -10; y <= 10; y++){
+			for (z = -10; z <= 10; z++){
+				x = 10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
-		for (y = -30; y <= 30; y++){
-			for (z = -30; z <= 30; z++){
-				x = -30;
+		for (y = -10; y <= 10; y++){
+			for (z = -10; z <= 10; z++){
+				x = -10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
-		for (x = -30; x <= 30; x++){
-			for (z = -30; z <= 30; z++){
-				y = -30;
+		for (x = -10; x <= 10; x++){
+			for (z = -10; z <= 10; z++){
+				y = -10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
-		for (x = -30; x <= 30; x++){
-			for (z = -30; z <= 30; z++){
-				y = 30;
+		for (x = -10; x <= 10; x++){
+			for (z = -10; z <= 10; z++){
+				y = 10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
-		for (x = -30; x <= 30; x++){
-			for (y = -30; y <= 30; y++){
-				z = 30;
+		for (x = -10; x <= 10; x++){
+			for (y = 10; y <= 10; y++){
+				z = -10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
-		for (x = -30; x <= 30; x++){
-			for (y = -30; y <= 30; y++){
-				z = -30;
+		for (x = -10; x <= 10; x++){
+			for (y = -10; y <= 10; y++){
+				z = -10;
 				x_temp = (float) x;
 				y_temp = (float) y;
 				z_temp = (float) z;
 
 				rotate(&x_temp, &y_temp, &z_temp, theta);
+				project(&x_temp, &y_temp, &z_temp, theta);
 			}
 		}
 		theta += 1.0f;
