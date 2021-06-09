f837904e0c76fb162e52db8bc872c87d39c63fc5
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-10 00:51:39 +0530

    now it renders something onto the screen Initial problem was that i was using wrong variable and passing it to flatten method

--- prototype/py/main.py
+++ prototype/py/main.py
@@ -5,37 +5,38 @@ import math
 from math import cos, sin
 
 ## CONSTANTS & VARIABLES DECLARATIONS
+
 DELAY: float = 1e-19
-SCR_DEPTH: int = 15
+SCR_DEPTH: int = 15 # from origin right?
 CUBE_SIZE:float = 6.0
-LIGHT_SRC: np.array = np.array([CUBE_SIZE + 1, CUBE_SIZE + 1, 20])
+LIGHT_SRC: np.array = np.array([CUBE_SIZE + 1, CUBE_SIZE + 1, 20])   
 PI: float = math.pi
-SCR_WIDTH: int = 60
-SCR_HEIGHT: int = 28 
-OFSET: np.array = np.array([8, 8, 0])
+SCR_WIDTH: int = 70
+SCR_HEIGHT: int = 22 
+OFSET: np.array = np.array([SCR_WIDTH/2, SCR_HEIGHT/2, 0])
 LUMINANCE:str = ".,-~:;=!*#$@"
 cosD = lambda x : cos(x*PI/180) 
 sinD = lambda x : sin(x*PI/180) 
-O_SCR: list = [' ' for i in range(SCR_HEIGHT*SCR_WIDTH)]
-O_POINT: np.array = np.array([CUBE_SIZE/2, CUBE_SIZE/2, 0])
+O_SCR: list = ['.' for i in range(SCR_HEIGHT*SCR_WIDTH)]
+O_POINT: np.array = np.array([SCR_WIDTH/2, SCR_HEIGHT/2, SCR_DEPTH + 4]) # 0.5*(3)^0.2
 
 ## GIVEN THE SIZE OF THE CUBE, Generate a cube
 def generate_cube(CUBE_SIZE:float):
 
     CUBE: list = [] 
-    for x in np.arange(0, CUBE_SIZE, 0.7):
-        for y in np.arange(0, CUBE_SIZE, 0.7):
-            z = 0
-            CUBE.append([np.array([x, y, z]), np.array([0, 0, -1])])
-            CUBE.append([np.array([x, z, y]), np.array([0, 0, -1])])
-            CUBE.append([np.array([z, y, x]), np.array([0, 0, -1])])
+    for x in np.arange(-CUBE_SIZE/2, CUBE_SIZE/2, 0.7):
+        for y in np.arange(-CUBE_SIZE/2, CUBE_SIZE/2, 0.7):
+            z = -CUBE_SIZE/2
+            CUBE.append(np.array([x, y, z]))
+            CUBE.append(np.array([x, z, y]))
+            CUBE.append(np.array([z, y, x]))
 
-    for x in np.arange(0, CUBE_SIZE, 0.7):
-        for y in np.arange(0, CUBE_SIZE, 0.7):
-            z = CUBE_SIZE
-            CUBE.append([np.array([x, y, z]), np.array([0, 0, -1])])
-            CUBE.append([np.array([x, z, y]), np.array([0, 0, -1])])
-            CUBE.append([np.array([z, y, x]), np.array([0, 0, -1])])
+    for x in np.arange(-CUBE_SIZE/2, CUBE_SIZE/2, 0.7):
+        for y in np.arange(-CUBE_SIZE/2, CUBE_SIZE, 0.7):
+            z = CUBE_SIZE/2
+            CUBE.append(np.array([x, y, z]))
+            CUBE.append(np.array([x, z, y]))
+            CUBE.append(np.array([z, y, x]))
 
     return np.array(CUBE)
 
@@ -56,8 +57,8 @@ def Rx(v:np.array, theta:float):
 
 def Ry(v:np.array, theta:float): 
     Ry_matrix:np.array  = np.array([
-        [cosD(theta), 0, sinD(theta)],
-        [0, 1, 0],
+        [cosD(theta), 0,  sinD(theta)],
+        [0          , 1,      0      ],
         [-sinD(theta), 0, cosD(theta)]
      ])
     res = np.matmul(Ry_matrix, v)
@@ -66,8 +67,8 @@ def Ry(v:np.array, theta:float):
 def Rz(v:np.array, theta:float): 
     Rz_matrix:np.array  = np.array([
         [cosD(theta), -sinD(theta), 0],
-        [sinD(theta), cosD(theta), 0],
-        [0, 0, 1]
+        [sinD(theta),  cosD(theta), 0],
+        [0          ,        0,     1]
      ])
     res = np.matmul(Rz_matrix, v)
     return res
@@ -82,11 +83,10 @@ def Project(v:np.array):
     x = v[0]
     y = v[1]
     z = v[2]
-    k:float = (z - O_POINT[2])/O_POINT[2]
+    k:float = (O_POINT[2] - SCR_DEPTH)/(O_POINT[2] - z)
     res = np.ones(2)
-    if z != 0:
-        res[0] = int(k*x/z)
-        res[1] = int(k*y/z)
+    res[0] = int(k*x)
+    res[1] = int(k*y)
 
     return res
 
@@ -121,7 +121,6 @@ def flatten(v:np.array):
 if __name__ == "__main__":
 
     theta:float = 1.0
-    r_pt: np.array = np.zeros(3)
 
     while True:
 
@@ -130,16 +129,15 @@ if __name__ == "__main__":
         sleep(DELAY)
         cube = generate_cube(CUBE_SIZE)
         for pt in cube:
-            rt_pt = translate(Rz(Ry(Rx(pt[0], theta), theta), theta), OFSET)
-            p_pt:np.array = Project(r_pt)
-            O_SCR[flatten(p_pt)] = '.'
-            l = luminance_calc(r_pt, O_POINT, pt[1])
+            rt_pt = translate(Rz(Ry(Rx(pt, theta), theta), theta), OFSET)
+            p_pt:np.array = Project(rt_pt)
+            O_SCR[flatten(p_pt)] = 'C'
         for k in range(SCR_WIDTH*SCR_HEIGHT):
             if k%SCR_WIDTH == 0:
-                print('\n')
+                print('\n', end='')
             else:
                 print(O_SCR[k], end = '')
 
-        O_SCR = [' ' for i in range(SCR_HEIGHT*SCR_WIDTH)]
+        O_SCR = ['.' for i in range(SCR_HEIGHT*SCR_WIDTH)]
          
         theta += 1.0
